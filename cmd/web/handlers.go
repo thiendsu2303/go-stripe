1730926441f4dc265341a7f1a278565845c6513d
thiendsu2303 <90961091+thiendsu2303@users.noreply.github.com>
2025-06-19 21:39:10 +0700

    adapt form to valid and success payment

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,8 +2,42 @@ package main
 
 import "net/http"
 
+// VirtualTerminal displays the virtual terminal page
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
+}
+
+// PaymentSucceeded displays the receipt page
+func (app *application) PaymentSucceeded(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		app.errorLog.Println(err)
+		return
+	}
+
+	// read posted data
+	cardHolder := r.Form.Get("cardholder_name")
+	email := r.Form.Get("cardholder_email")
+	paymentIntent := r.Form.Get("payment_intent")
+	paymentMethod := r.Form.Get("payment_method")
+	paymentAmount := r.Form.Get("payment_amount")
+	paymentCurrency := r.Form.Get("payment_currency")
+
+	data := make(map[string]interface{})
+	data["cardholder"] = cardHolder
+	data["email"] = email
+	data["pi"] = paymentIntent
+	data["pm"] = paymentMethod
+	data["pa"] = paymentAmount
+	data["pc"] = paymentCurrency
+
+	// should write this data to session, and then redirect user to new page?
+
+	if err := app.renderTemplate(w, r, "succeeded", &templateData{
+		Data: data,
+	}); err != nil {
 		app.errorLog.Println(err)
 	}
 }
